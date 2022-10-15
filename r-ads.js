c3e96fe45a82fbe93d5a52d880a03cbc4e4eb309
dimaslanjaka <dimaslanjaka@gmail.com>
2022-10-16 00:11:11 +0700

    update

---
+++ r-ads.js
@@ -0,0 +1,6 @@
+const pubs = ['2974518380815858','0168582118085122']
+
+/*<script
+  async
+  src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-0168582118085122"
+  crossorigin="anonymous"></script>*/
