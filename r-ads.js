44961c5f2590b82cab2b4065e9cf47c75423d451
Dimas Lanjaka <dimaslanjaka@gmail.com>
2022-12-28 00:34:36 +0700

    Update r-ads.js

--- r-ads.js
+++ r-ads.js
@@ -1,6 +1,7 @@
 (function(){
 	const script = document.createElement('script');
 	script.src = 'https://www.webmanajemen.com/page/assets/js/r-ads.js';
+	script.async = false;
 	const place = document.body || document.head;
 	place.append(script);
 })();
