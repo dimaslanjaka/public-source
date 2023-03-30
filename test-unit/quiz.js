ef058a17cfd12c0aeb8b8a623f18f9603a7ddfeb
dimaslanjaka <dimaslanjaka@gmail.com>
2023-03-30 13:08:38 +0700

    hotfix new quiz layout

--- test-unit/quiz.js
+++ test-unit/quiz.js
@@ -71,6 +71,19 @@ Promise.all(
   onlyO.addEventListener('change', doSearch);
 
   document.getElementById('nosubmit').addEventListener('submit', doSearch);
+  document.getElementById('addQuiz').addEventListener('submit', function (e) {
+    e.preventDefault();
+    // const form = document.querySelector('form');
+    const formData = new FormData(this);
+    const payload = JSON.stringify(formDataToObject(formData));
+    fetch(this.action, {
+      method: this.method,
+      body: payload,
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+  });
 });
 
 /**
@@ -114,4 +127,75 @@ function createLi(listData) {
     // Reset the list item
     listItem = document.createElement('li');
   }
+}
+
+function formDataToObject(formData) {
+  let object = {};
+
+  const debug = (message) => {
+    console.log(message);
+  };
+
+  /**
+   * Parses FormData key xxx`[x][x][x]` fields into array
+   */
+  const parseKey = (key) => {
+    const subKeyIdx = key.indexOf('[');
+
+    if (subKeyIdx !== -1) {
+      const keys = [key.substring(0, subKeyIdx)];
+      key = key.substring(subKeyIdx);
+
+      for (const match of key.matchAll(/\[(?<key>.*?)]/gm)) {
+        keys.push(match.groups.key);
+      }
+      return keys;
+    } else {
+      return [key];
+    }
+  };
+
+  /**
+   * Recursively iterates over keys and assigns key/values to object
+   */
+  const assign = (keys, value, object) => {
+    const key = keys.shift();
+    debug(key);
+    debug(keys);
+
+    // When last key in the iterations
+    if (key === '' || key === undefined) {
+      return object.push(value);
+    }
+
+    if (Reflect.has(object, key)) {
+      debug('hasKey ' + key);
+      // If key has been found, but final pass - convert the value to array
+      if (keys.length === 0) {
+        if (!Array.isArray(object[key])) {
+          debug('isArray ' + object[key]);
+          object[key] = [object[key], value];
+          return;
+        }
+      }
+      // Recurse again with found object
+      return assign(keys, value, object[key]);
+    }
+
+    // Create empty object for key, if next key is '' do array instead, otherwise set value
+    if (keys.length >= 1) {
+      debug(`undefined '${key}' key: remaining ${keys.length}`);
+      object[key] = keys[0] === '' ? [] : {};
+      return assign(keys, value, object[key]);
+    } else {
+      debug('set value: ' + value);
+      object[key] = value;
+    }
+  };
+
+  for (const pair of formData.entries()) {
+    assign(parseKey(pair[0]), pair[1], object);
+  }
+
+  return object;
 }
